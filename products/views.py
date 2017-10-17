b7e34b12d72354a7bb4fba7de0fa494c3297eb64
Pablo Reina López <pab993@gmail.com>
2017-10-17 20:20:53 +0200

    Correcciones
    Arreglados algunos errores de paginación

--- products/views.py
+++ products/views.py
@@ -52,7 +52,17 @@ def login_user(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    products = Product.objects.all()
+                    products_list = Product.objects.all()
+                    paginator = Paginator(products_list, 5)
+                    page = request.GET.get('page')
+                    try:
+                        products = paginator.page(page)
+                    except PageNotAnInteger:
+                        # If page is not an integer, deliver first page.
+                        products = paginator.page(1)
+                    except EmptyPage:
+                        # If page is out of range (e.g. 9999), deliver last page of results.
+                        products = paginator.page(paginator.num_pages)
                     context = {"products": products}
                     return render(request, 'products/index.html', context)
                 else:
